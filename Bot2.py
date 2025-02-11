3bd99cd760aefa245195ef0db02f2f2859455f40
Gabriel Arturo Guzman <93060120+GabrielGUGU@users.noreply.github.com>
2025-02-10 20:47:05 -0500

    Update Bot2.py

--- Bot2.py
+++ Bot2.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -20,6 +21,24 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+bot.run("")
+ 
     """Adds two numbers together."""
     await ctx.send(left + right)
 
